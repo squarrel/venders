d695f440668cf3def38c59669f6b2600932ab731
Milos <milosbulatovic3@gmail.com>
2021-09-08 13:57:33 +0200

    add user profile app with roles

---
+++ user_profile/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    BUYER = 'buyer'
+    SELLER = 'seller'
+    ROLES = [
+        (BUYER, 'Buyer'),
+        (SELLER, 'Seller'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=18, choices=ROLES, default=BUYER)
