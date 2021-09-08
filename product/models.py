fcc7166795dfd26e65d64c354f15b74cce1f4ced
Milos <milosbulatovic3@gmail.com>
2021-09-08 15:20:30 +0200

    add product app

---
+++ product/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=55)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount_available = models.IntegerField(default=0)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
