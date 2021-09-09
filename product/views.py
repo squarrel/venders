cb1b1ba2816935da9fea640f13e67872060e1396
Milos <milosbulatovic3@gmail.com>
2021-09-09 12:00:05 +0200

    add view. get and post methods

--- product/views.py
+++ product/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class ProductRecordView(APIView):
+    def get(self, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
