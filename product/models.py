7e8043c9e66904933947ca0b5d771f85e490442d
Milos <milosbulatovic3@gmail.com>
2021-09-09 08:35:47 +0200

    add default to cost field

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,4 +7,4 @@ class Product(models.Model):
     product_name = models.CharField(max_length=55)
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     amount_available = models.IntegerField(default=0)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
