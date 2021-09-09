eb64115e30657c4a2aa29497c38620408593741e
Milos <milosbulatovic3@gmail.com>
2021-09-09 08:32:01 +0200

    add view. get and post methods

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from user_profile.models import UserProfile
+from user_profile.serializers import UserProfileSerializer
+
+
+class UserProfileRecordView(APIView):
+    def get(self, format=None):
+        user_profiles = UserProfile.objects.all()
+        serializer = UserProfileSerializer(user_profiles, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserProfileSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
