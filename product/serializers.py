262f14234cf304f298a37ac3d2b31c1dfa62241b
Milos <milosbulatovic3@gmail.com>
2021-09-09 12:00:46 +0200

    add serializer

---
+++ product/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from product.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    seller = serializers.CharField(source='seller.email')
+
+    class Meta:
+        model = Product
+        fields = ['product_name', 'seller', 'amount_available', 'cost']
+
+    def create(self, validated_data):
+        seller_data = validated_data.pop('seller')
+        #seller = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        user = User.objects.get(email=seller_data)
+        product, created = Product.objects.update_or_create(
+            seller=user,
+            product_name=validated_data.pop('product_name'),
+            amount_available=validated_data.pop('amount_available'),
+            cost=validated_data.pop('cost'),
+        )
+        return product
