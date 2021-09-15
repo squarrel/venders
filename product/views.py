8d0e144d08f6900a4fb3ac790aaebd1aea961bac
Milos <milosbulatovic3@gmail.com>
2021-09-15 06:13:23 +0200

    add put, delete methods

--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,6 +9,12 @@ from product.serializers import ProductSerializer
 
 
 class ProductRecordView(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
     def get(self, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
@@ -23,3 +30,16 @@ class ProductRecordView(APIView):
 
         return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = self.get_object(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
