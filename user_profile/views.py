78b47b2749c051f20debf0bc007ba3f67bec6745
Milos <milosbulatovic3@gmail.com>
2021-09-15 04:58:51 +0200

    add put and delete methods. amend coin validation.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
@@ -10,7 +10,13 @@ from user_profile.models import UserProfile
 from user_profile.serializers import UserProfileSerializer
 
 
-class UserProfileRecordView(APIView):
+class UserProfileView(APIView):
+    def get_object(self, pk):
+        try:
+            return UserProfile.objects.get(pk=pk)
+        except UserProfile.DoesNotExist:
+            raise Http404
+
     def get(self, format=None):
         user_profiles = UserProfile.objects.all()
         serializer = UserProfileSerializer(user_profiles, many=True)
@@ -26,11 +32,24 @@ class UserProfileRecordView(APIView):
 
         return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user_profile = self.get_object(pk=pk)
+        user_profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @csrf_exempt
 def deposit(request, pk, amount):
-    # perhaps vending machines validate amount on their own, so this isn't needed
-    if amount % 5 != 0:
+    if amount not in UserProfile.ALLOWED_COINS:
         return JsonResponse({'message': 'Invalid amount.'})
 
     user_profile = UserProfile.objects.get(user__pk=pk)
