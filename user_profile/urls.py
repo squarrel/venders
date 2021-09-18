5220948d586cb3172bda697a6dde9d96b9262acd
Milos <milosbulatovic3@gmail.com>
2021-09-18 10:23:29 +0200

    fix deposit and buy paths

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,6 +5,6 @@ from user_profile import views
 urlpatterns = [
     path('', views.UserProfileView.as_view()),
     path('<int:pk>/', views.UserProfileView.as_view()),
-    path('deposit/<int:pk>/<int:amount>/', views.deposit),
-    path('buy/<int:pk>/<int:amount>/', views.buy),
+    path('deposit/<int:amount>/', views.deposit),
+    path('buy/<int:product_id>/<int:amount>/', views.buy),
 ]
