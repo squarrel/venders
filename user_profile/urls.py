867541d987532bbe262d0a72d16d60f85775a757
Milos <milosbulatovic3@gmail.com>
2021-09-20 14:22:49 +0200

    remove params from path

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.UserProfileView.as_view()),
     path('<int:pk>/', views.UserProfileView.as_view()),
     path('deposit/<int:amount>/', views.deposit),
-    path('buy/<int:product_id>/<int:amount>/', views.buy),
+    path('buy/', views.buy),
 ]
