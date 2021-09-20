f65453d090feb57b779de177e86e280df08fde92
Milos <milosbulatovic3@gmail.com>
2021-09-20 16:38:22 +0200

    change cost field to integer

--- product/models.py
+++ product/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     product_name = models.CharField(max_length=55)
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     amount_available = models.IntegerField(default=0)
-    cost = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
+    cost = models.IntegerField(default=0)
