24013081281521c9569f609715514fce24220c85
Milos <milosbulatovic3@gmail.com>
2021-09-21 06:52:49 +0200

    add name for path deposit

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,7 +5,7 @@ from user_profile import views
 urlpatterns = [
     path('', views.UserProfileView.as_view()),
     path('<int:pk>/', views.UserProfileView.as_view()),
-    path('deposit/<int:amount>/', views.deposit),
+    path('deposit/<int:amount>/', views.deposit, name='deposit'),
     path('buy/', views.buy),
     path('reset/', views.reset),
 ]
